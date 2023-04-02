139041d68aa1ddbf0211213db5ce1e3c4ab4d4eb
xdark-git <konteyemouhamadou@gmail.com>
2023-04-02 21:27:34 +0000

    adding build method

--- lib/Viewpoint/AdminBundle/ViewpointAdminBundle.php
+++ lib/Viewpoint/AdminBundle/ViewpointAdminBundle.php
@@ -1,9 +1,13 @@
 <?php
 namespace Viewpoint\AdminBundle;
 
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class ViewpointAdminBundle extends Bundle
 {
-
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
 }
