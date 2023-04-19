e40a69904bcf9d9fd5e40d9b516a663400f7ac8f
xdark-git <konteyemouhamadou@gmail.com>
2023-04-19 01:46:44 +0000

    using themeresolver to get theme prefix

--- lib/Viewpoint/ThemeBundle/Controller/RegisterController.php
+++ lib/Viewpoint/ThemeBundle/Controller/RegisterController.php
@@ -5,12 +5,13 @@ namespace Viewpoint\ThemeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Viewpoint\ThemeBundle\Service\ThemeResolver;
 
 class RegisterController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function login (): Response
-    {
-        return $this->render('@ViewpointTheme/cms/login.html.twig');
+    public function login (ThemeResolver $themeResolver): Response
+    {   
+        return $this->render($themeResolver->getThemePathPrefix().'/cms/login.html.twig');
     }
 }
