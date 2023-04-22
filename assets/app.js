44a83f9c53bf2b6ce9bdeabf9ee716fead180815
xdark-git <konteyemouhamadou@gmail.com>
2023-04-22 15:06:28 +0000

    adding and importing font-awesome

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free/scss/solid.scss';
+import '@fortawesome/fontawesome-free/scss/brands.scss';
+
 // start the Stimulus application
-import './bootstrap';
+// import './bootstrap';
