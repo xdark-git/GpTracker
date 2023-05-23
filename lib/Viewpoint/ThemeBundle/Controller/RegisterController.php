de9b7a21d5cf28f344f966a70ee6ed44d47b5f39
xdark-git <konteyemouhamadou@gmail.com>
2023-05-24 00:26:07 +0100

    Add line breaks for improved code readability

--- lib/Viewpoint/ThemeBundle/Controller/RegisterController.php
+++ lib/Viewpoint/ThemeBundle/Controller/RegisterController.php
@@ -14,31 +14,37 @@ class RegisterController extends AbstractController
     {   
         return $this->render($themeResolver->getThemePathPrefix('/core/login.html.twig'));
     }
+
     #[Route('/register', name: 'app_register')]
     public function register (ThemeResolver $themeResolver): Response
     {   
         return $this->render($themeResolver->getThemePathPrefix('/core/register.html.twig'));
     }
+
     #[Route('/', name: 'app_home')]
     public function home (ThemeResolver $themeResolver): Response
     {   
         return $this->render($themeResolver->getThemePathPrefix('/core/home.html.twig'));
     }
+
     #[Route('/rooms', name: 'app_rooms')]
     public function rooms (ThemeResolver $themeResolver): Response
     {   
         return $this->render($themeResolver->getThemePathPrefix('/core/rooms.html.twig'));
     }
+
     #[Route("/informations", name:"app_informations")]
     public function informations(ThemeResolver $themeResolver): Response
     {
         return $this->render($themeResolver->getThemePathPrefix('/core/informations/contents/account.html.twig'));
     }
+
     #[Route("/informations/account", name:"app_informations_user")]
     public function informationUser(ThemeResolver $themeResolver): Response
     {
         return $this->render($themeResolver->getThemePathPrefix('/core/informations/contents/account.html.twig'));
     }
+    
     #[Route("/informations/empty-message", name:"app_empty_message")]
     public function emptyMessage (ThemeResolver $themeResolver): Response
     {
