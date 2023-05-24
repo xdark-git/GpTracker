8bc6bc147133b66df1ae121504cdfba83fb2d6d2
xDev <papesock@yahoo.fr>
2023-05-24 20:10:51 +0200

    fix conflict v2

--- lib/Viewpoint/ThemeBundle/Controller/RegisterController.php
+++ lib/Viewpoint/ThemeBundle/Controller/RegisterController.php
@@ -38,10 +38,8 @@ class RegisterController extends AbstractController
     public function emptyMessage (ThemeResolver $themeResolver): Response
     {
         return $this->render($themeResolver->getThemePathPrefix('/core/informations/contents/emptyMessage.html.twig'));
-    
     }
-
-    #[Route('/error', name: 'app_home')]
+    #[Route('/error', name: 'app_error')]
     public function notFoundPage (ThemeResolver $themeResolver): Response
     {   
         return $this->render($themeResolver->getThemePathPrefix('/core/error.html.twig'));
