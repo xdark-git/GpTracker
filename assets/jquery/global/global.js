b69b743a16c7115d687b444b1a73a8814d9d667b
xdark-git <konteyemouhamadou@gmail.com>
2023-05-24 23:55:38 +0100

    new file

---
+++ assets/jquery/global/global.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+/*----------------------------------
+    TRANSFORM ROOM DIV TO A TAG
+ -----------------------------------*/
+
+$(function () {
+    var $container = $("#room-list-container");
+    console.log($container.lenght);
+});
