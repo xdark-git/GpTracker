45298015454b2c457f264f3c9adeb3c2c737f050
xDev <papesock@yahoo.fr>
2023-05-28 20:33:17 +0200

    change name

--- lib/Viewpoint/ThemeBundle/Controller/RegisterController.php
+++ lib/Viewpoint/ThemeBundle/Controller/RegisterController.php
@@ -39,12 +39,12 @@ class RegisterController extends AbstractController
     {
         return $this->render($themeResolver->getThemePathPrefix('/core/informations/contents/emptyMessage.html.twig'));
     }
-    #[Route("/informations/empty-package", name:"app_empty_message")]
+    #[Route("/informations/empty-package", name:"app_empty_package")]
     public function emptyPackage (ThemeResolver $themeResolver): Response
     {
         return $this->render($themeResolver->getThemePathPrefix('/core/informations/contents/emptyPackage.html.twig'));
     }
-    #[Route("/informations/empty-order", name:"app_empty_message")]
+    #[Route("/informations/empty-order", name:"app_empty_order")]
     public function emptyOrder (ThemeResolver $themeResolver): Response
     {
         return $this->render($themeResolver->getThemePathPrefix('/core/informations/contents/emptyOrder.html.twig'));
