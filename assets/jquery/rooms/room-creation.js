8b47474fc987d35f76a9bb42df9012889e38fb09
xdark-git <konteyemouhamadou@gmail.com>
2023-06-02 02:45:54 +0100

    room creation form jquery fonctionalities

--- assets/jquery/rooms/room-creation.js
+++ assets/jquery/rooms/room-creation.js
@@ -7,30 +7,101 @@ $(function () {
     var currentTab = 0;
     showTab(currentTab);
 
-    function showTab(n) {
-        
-        var $form = $("#roomCreationForm");
-        var $tabs = $form.find(".tab");
-        var $previousBtn = $form.find("#btn-previous");
-        var $nextBtn = $form.find("#btn-next");
-        var $previewBtn = $form.find("#btn-preview");
-        var $submitBtn = $form.find("#btn-submit");
+    var $form = $("#roomCreationForm");
+    var $nextBtn = $form.find("#btn-next");
+    var $previousBtn = $form.find("#btn-previous");
 
-        $tabs.eq(n).removeClass("hidden");
+    $nextBtn.on("click", function () {
+        // move to the next tab
+        currentTab = nextPrevious(currentTab);
+        showTab(currentTab);
+    });
+    $previousBtn.on("click", function () {
+        // return to the previous tab
+        if (currentTab > 0) {
+            currentTab--;
+            showTab(currentTab);
+        }
+    });
+});
 
-        if (n == 0) {
-            $previousBtn.addClass("hidden");
+function showTab(n) {
+    var $form = $("#roomCreationForm");
+    var $tabs = $form.find(".tab");
+    var $previousBtn = $form.find("#btn-previous");
+    var $nextBtn = $form.find("#btn-next");
+    var $previewBtn = $form.find("#btn-preview");
+    var $submitBtn = $form.find("#btn-submit");
+
+    // Show the current tab and hide other tabs
+    $tabs.each(function () {
+        if ($tabs.index(this) == n) {
+            $(this).removeClass("hidden");
         } else {
-            $previousBtn.removeClass("hidden");
+            $(this).addClass("hidden");
         }
-        if (n < $tabs.length - 1) {
-            $nextBtn.removeClass("hidden");
-            $previewBtn.addClass("hidden");
-        } else {
-            $nextBtn.addClass("hidden");
-            $previewBtn.removeClass("hidden");
-        }
+    });
+    // Show or Hide the previous button on the first tab
+    if (n == 0) {
+        $previousBtn.addClass("hidden");
+    } else {
+        $previousBtn.removeClass("hidden");
+    }
+    if (n < $tabs.length - 1) {
+        // Show the next button and hide the preview button on tabs before the last one
+        $nextBtn.removeClass("hidden");
+        $previewBtn.addClass("hidden");
+    } else {
+         // Hide the next button and show the preview button on the last tab
+        $nextBtn.addClass("hidden");
+        $previewBtn.removeClass("hidden");
     }
 
-    function fixStepIndicator(n) {}
-});
+    fixStepIndicator(n);
+}
+
+function nextPrevious(n) {
+    var $form = $("#roomCreationForm");
+    var $tabs = $form.find(".tab");
+
+    if (validForm(n, $tabs)) {
+         // Increment the currentTab variable if the form is valid and not on the last tab
+        return n < $tabs.length - 1 ? (n = n + 1) : n;
+    } else {
+        return n;
+    }
+}
+
+function validForm(n, $tabs) {
+    var valid = true;
+    var $inputs = $tabs.eq(n).find("input");
+    $inputs.each(function () {
+        if ($(this).attr("id") !== "secondary-tel" && $(this).val() == "") {
+            $(this).siblings("label").addClass("error");
+            valid = false;
+        } else {
+            $(this).siblings("label").removeClass("error");
+        }
+    });
+    return valid;
+}
+
+function fixStepIndicator(n) {
+    var $nav = $("main .gp-room-creation > .content").find("> .nav");
+    var $navList = $nav.find("li");
+    var $stepIndicator = $("#roomCreationForm").find("> .form-title .step");
+
+    $navList.each(function () {
+        if ($(this).index() == n && !$(this).hasClass("current")) {
+            // Add "current" class to the current step in the navigation
+            $(this).addClass("current");
+            // Update the step indicator with the current step and total steps
+            $stepIndicator.html(n + 1 + "/" + $navList.length + " :");
+        } else if ($(this).index() == n && $(this).hasClass("current")) {
+            $stepIndicator.html(n + 1 + "/" + $navList.length + " :");
+        } else {
+            // Remove "current" class from other steps in the navigation
+            $(this).removeClass("current");
+        }
+    });
+}
