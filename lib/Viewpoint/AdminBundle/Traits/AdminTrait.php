14eb8d3d64590aad86a75e2bfe594835dd375a44
xdark-git <konteyemouhamadou@gmail.com>
2023-07-01 14:08:33 +0100

    updating trait & typing methods

--- lib/Viewpoint/AdminBundle/Traits/AdminTrait.php
+++ lib/Viewpoint/AdminBundle/Traits/AdminTrait.php
@@ -6,18 +6,25 @@ use Viewpoint\AdminBundle\Entity\User;
 
 trait AdminTrait{
 
-    public function createRole(string $name){
+    public function createRole(string $name): Role
+    {
         $role = new Role();
         $role->setName($name);
 
         return $role;
     }
     
-    public function createUser(string $email, string $password, Role $role){
+    public function createUser(string $email, string $password, Role $role): User
+    {
         $user = new User();
-        $user->setEmail($email)
-            ->setPassword($password)
-            ->setRole($role);
+        $user->setEmail($email)->setRole($role);
+
+        $hashedPassword = $this->passwordHasher->hashPassword(
+            $user,
+            $password
+        );
+
+        $user->setPassword($hashedPassword);
 
         return $user;
         
