fb8528ecc1167b856c8e936bc56f0adb2b1e7d71
xdark-git <konteyemouhamadou@gmail.com>
2023-07-01 14:13:24 +0100

    new role and user fixture

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,41 @@
 
 namespace App\DataFixtures;
 
+use Faker\Factory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Viewpoint\AdminBundle\Traits\AdminTrait;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 
-class AppFixtures extends Fixture
+class AppFixtures extends Fixture implements FixtureGroupInterface
 {
+    use AdminTrait;
+
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+        
+    }
+
+    public static function getGroups(): array
+    {
+        return ['dev'];
+    }
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
+        $roleAdmin = $this->createRole("ROLE_ADMIN");
+        $roleUser = $this->createRole("ROLE_USER");
+        $manager->persist($roleAdmin);
+        $manager->persist($roleUser);
+        $manager->flush();
+
+        $faker = Factory::create();
+        
+        $user1 = $this->createUser(email: $faker->email, password: "test", role: $roleAdmin);
+        $manager->persist($user1);
         $manager->flush();
     }
 }
