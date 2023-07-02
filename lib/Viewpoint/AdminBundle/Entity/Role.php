3aaa8064a93bb30f42abf601aa1a2d91fbb2445a
xdark-git <konteyemouhamadou@gmail.com>
2023-07-02 22:51:20 +0100

    adding validation

--- lib/Viewpoint/AdminBundle/Entity/Role.php
+++ lib/Viewpoint/AdminBundle/Entity/Role.php
@@ -9,6 +9,7 @@ use Doctrine\Common\Collections\Collection;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Mapping\OneToMany;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: RoleRepository::class)]
 class Role
@@ -19,13 +20,21 @@ class Role
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 60, nullable:false, unique:true)]
+    #[ORM\Column(length: 60, nullable: false, unique: true)]
+    #[
+        Assert\Sequentially([
+            new Assert\NotNull(),
+            new Assert\Type("string"),
+            new Assert\Length(max: 60),
+        ])
+    ]
     private ?string $name = null;
 
-    #[OneToMany(targetEntity: User::class, mappedBy: 'role')]
+    #[OneToMany(targetEntity: User::class, mappedBy: "role")]
     private Collection $users;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->users = new ArrayCollection();
     }
 
