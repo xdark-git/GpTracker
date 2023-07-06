5166b0d055ef2b6d8c4985b99494e9effcc5b186
xdark-git <konteyemouhamadou@gmail.com>
2023-07-06 14:46:19 +0100

    adding login form type

---
+++ lib/Viewpoint/AdminBundle/Form/LoginFormType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Viewpoint\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class LoginFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add("email", EmailType::class);
+    }
+
+}
