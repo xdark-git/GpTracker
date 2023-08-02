461c0ac9fb26f3cda2b6552a2646c721a092e569
xdark-git <konteyemouhamadou@gmail.com>
2023-08-02 01:42:53 +0100

    adding Room repository

---
+++ lib/Viewpoint/ThemeBundle/Repository/RoomRepository.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Viewpoint\ThemeBundle\Repository;
+
+use Viewpoint\ThemeBundle\Entity\Room;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Room>
+ *
+ * @method Room|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Room|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Room[]    findAll()
+ * @method Room[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RoomRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Room::class);
+    }
+
+    public function save(Room $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Room $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+}
