6c73a2046f49dc0cd0e02f194f31a431103bbdf4
xdark-git <konteyemouhamadou@gmail.com>
2023-08-05 17:56:44 +0100

    changing input types

--- lib/Viewpoint/ThemeBundle/Form/RoomCellularFormType.php
+++ lib/Viewpoint/ThemeBundle/Form/RoomCellularFormType.php
@@ -3,6 +3,7 @@
 namespace Viewpoint\ThemeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,11 +14,11 @@ class RoomCellularFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add("primary", TextType::class, [
+            ->add("primary", TelType::class, [
                 "label" => "Numéro de téléphone WhatsApp",
                 "help" => "Veuillez entrer un format valide, par exemple : +1 XXX XXX XXXX",
             ])
-            ->add("secondary", TextType::class, [
+            ->add("secondary", TelType::class, [
                 "label" => "Numéro de téléphone secondaire",
                 "help" => "Veuillez entrer un format valide, par exemple : +1 XXX XXX XXXX",
                 "required" => false,
