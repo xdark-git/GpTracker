e47c2f6085adfdee8014b558d73de51221419d73
xdark-git <konteyemouhamadou@gmail.com>
2023-08-13 22:28:22 +0100

    new fixtures data

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -8,6 +8,8 @@ use Doctrine\Persistence\ObjectManager;
 use Viewpoint\AdminBundle\Traits\AdminTrait;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Viewpoint\ThemeBundle\Entity\Room;
+use Viewpoint\ThemeBundle\Entity\RoomCellular;
 use Viewpoint\ThemeBundle\Traits\ThemeTrait;
 
 class AppFixtures extends Fixture implements FixtureGroupInterface
@@ -37,12 +39,28 @@ class AppFixtures extends Fixture implements FixtureGroupInterface
 
         $user1 = $this->createUser(
             email: "test@gmail.com",
-            username: $faker->userName,
+            username: 'gpadmin1',
+            password: "test",
+            role: $roleAdmin,
+            isVerified: true
+        );
+        $user2 = $this->createUser(
+            email: $faker->email,
+            username: 'gpadmin2',
+            password: "test",
+            role: $roleAdmin,
+            isVerified: true
+        );
+        $user3 = $this->createUser(
+            email: $faker->email,
+            username: 'gpadmin3',
             password: "test",
             role: $roleAdmin,
             isVerified: true
         );
         $manager->persist($user1);
+        $manager->persist($user2);
+        $manager->persist($user3);
         $manager->flush();
 
         /* CONVEYANCE CREATION */
@@ -54,6 +72,38 @@ class AppFixtures extends Fixture implements FixtureGroupInterface
         $manager->persist($conveyance2);
         $manager->persist($conveyance3);
         $manager->flush();
-        
+
+        /* ROOM CREATION */
+        $users = [$user1, $user2, $user3];
+        $conveyances = [$conveyance1, $conveyance2, $conveyance3];
+
+        for ($i = 0; $i < 200; $i++) {
+            $randomUser = array_rand($users);
+            $randomConveyance = array_rand($conveyances);
+            
+            $room = new Room();
+
+            $room
+                ->setName($faker->word)
+                ->setCurrency("XOF")
+                ->setUnitPrice($faker->numberBetween(1000, 5000))
+                ->setWeight($faker->numberBetween(10, 100))
+                ->setDepartureLocation($faker->city)
+                ->setArrivalLocation($faker->city)
+                ->setDepartureDate($faker->dateTimeBetween("now", "+1 week"))
+                ->setArrivalDate($faker->dateTimeBetween("+1 week", "+1 month"))
+                ->setUser($users[$randomUser])
+                ->setConveyance($conveyances[$randomConveyance]);
+            
+            $cellular = new RoomCellular();
+            $cellular
+                ->setPrimary($faker->phoneNumber)
+                ->setSecondary($faker->phoneNumber)
+                ->setRoom($room);
+
+            $manager->persist($room);
+            $manager->persist($cellular);
+            $manager->flush();
+        }
     }
 }
