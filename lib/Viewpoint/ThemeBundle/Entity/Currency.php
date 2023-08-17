b72f9bbfe979ccbdfc24f817f3ba537f0489d2b0
xdark-git <konteyemouhamadou@gmail.com>
2023-08-17 15:19:09 +0100

    new entity

---
+++ lib/Viewpoint/ThemeBundle/Entity/Currency.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Viewpoint\ThemeBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+use Viewpoint\ThemeBundle\Repository\CurrencyRepository;
+
+#[ORM\Entity(repositoryClass: CurrencyRepository::class)]
+class Currency
+{
+    use TimestampableEntity;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 3)]
+    private ?string $code = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $symbol = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): self
+    {
+        $this->code = $code;
+        return $this;
+    }
+
+    public function getSymbol(): ?string
+    {
+        return $this->symbol;
+    }
+
+    public function setSymbol(?string $symbol): self
+    {
+        $this->symbol = $symbol;
+        return $this;
+    }
+}
