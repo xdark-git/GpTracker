5a41f90f5d72e616049e44465706fac84be8c8c5
xdark-git <konteyemouhamadou@gmail.com>
2023-08-18 20:21:52 +0100

    passing in url param and reloading page

--- assets/jquery/rooms/room.js
+++ assets/jquery/rooms/room.js
@@ -1,14 +1,26 @@
 /*--------------------------------------------
-   SUBMIT SORT FORM ON CLICK
+   SUBMIT SORT FORM ON CLICK 
  --------------------------------------------*/
 $(function () {
-    // Get the select element
-    var $sortForm = $('#sort-form');
-    var $select = $("#filter-select");
+ 
+    const sortSelect = $("#filter-select");
 
-    // Listen for change event on select
-    $select.on("change", function () {
-        // Submit the form when an option is selected
-        $sortForm.trigger('submit');
+    // Listen for changes to the sort select
+    sortSelect.on("change", function() {
+        // Get the selected value
+        const selectedSort = sortSelect.val();
+
+        // Build the new URL with the selected sort value
+        const newUrl = updateUrlParameter(window.location.href, "room_sort[sort]", selectedSort);
+
+        // Redirect to the new URL
+        window.location.href = newUrl;
     });
+
+    // Function to update a parameter in the URL
+    function updateUrlParameter(url, key, value) {
+        const urlObj = new URL(url);
+        urlObj.searchParams.set(key, value);
+        return urlObj.toString();
+    }
 });
