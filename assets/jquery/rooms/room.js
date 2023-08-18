37ef45656250b5676d1b46c728a1f8169b5413cc
xdark-git <konteyemouhamadou@gmail.com>
2023-08-18 19:56:26 +0100

    removing search form trigger

--- assets/jquery/rooms/room.js
+++ assets/jquery/rooms/room.js
@@ -4,13 +4,11 @@
 $(function () {
     // Get the select element
     var $sortForm = $('#sort-form');
-    var $searchForm = $('#search-form');
     var $select = $("#filter-select");
 
     // Listen for change event on select
     $select.on("change", function () {
         // Submit the form when an option is selected
         $sortForm.trigger('submit');
-        $searchForm.trigger('submit');
     });
 });
