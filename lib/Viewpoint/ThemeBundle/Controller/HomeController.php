c596885075b577ccaa1db3d93668d2375e130115
xDev <papesock@yahoo.fr>
2023-08-19 00:11:42 +0200

    add contact route

--- lib/Viewpoint/ThemeBundle/Controller/HomeController.php
+++ lib/Viewpoint/ThemeBundle/Controller/HomeController.php
@@ -4,11 +4,13 @@ namespace Viewpoint\ThemeBundle\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Viewpoint\ThemeBundle\Service\ThemeResolver;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Viewpoint\ThemeBundle\Entity\City;
 use Viewpoint\ThemeBundle\Entity\Room;
+use Viewpoint\ThemeBundle\Form\ContactFormType;
 use Viewpoint\ThemeBundle\Form\SearchFormType;
 use Viewpoint\ThemeBundle\Repository\RoomRepository;
 
@@ -36,6 +38,21 @@ class HomeController extends AbstractController
             "cities" => $cities
         ]);
     }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(Request $request): Response
+    {
+        $contactForm = $this->createForm(ContactFormType::class);
+        $contactForm->handleRequest($request);
+
+        if($contactForm->isSubmitted() && $contactForm->isValid()){
+            dd($contactForm->getData());
+        }
+
+        return $this->render($this->themeResolver->getThemePathPrefix('/core/contact.html.twig'), [
+            "contactForm" => $contactForm
+        ]);
+    }    
 
     #[Route("/code-of-conduct", name: "app_Code_of_Conduct")]
     public function CodeOfConduct(): Response
