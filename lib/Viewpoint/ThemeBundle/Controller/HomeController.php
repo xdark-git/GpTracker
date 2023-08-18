293a6a8a469856696cb4bb32257fde05bfc9daa6
xdark-git <konteyemouhamadou@gmail.com>
2023-08-18 19:22:59 +0100

    making search form dynamic

--- lib/Viewpoint/ThemeBundle/Controller/HomeController.php
+++ lib/Viewpoint/ThemeBundle/Controller/HomeController.php
@@ -7,25 +7,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Viewpoint\ThemeBundle\Service\ThemeResolver;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Viewpoint\ThemeBundle\Entity\City;
 use Viewpoint\ThemeBundle\Entity\Room;
+use Viewpoint\ThemeBundle\Form\SearchFormType;
 use Viewpoint\ThemeBundle\Repository\RoomRepository;
 
 class HomeController extends AbstractController
 {
     public function __construct(
         private ThemeResolver $themeResolver,
-        private EntityManagerInterface $entitymanager
+        private EntityManagerInterface $entityManager
     ) {
     }
     #[Route("/", name: "app_home")]
     public function index(): Response
     {
         /** @var RoomRepository */ 
-        $roomRepository = $this->entitymanager->getRepository(Room::class);
+        $roomRepository = $this->entityManager->getRepository(Room::class);
         $rooms = $roomRepository->findAvailableRoomForHomePage();
-        
+
+        $searchForm = $this->createForm(type: SearchFormType::class, options: [
+            'action' => $this->generateUrl('app_rooms')
+        ]);
+        $cities = $this->entityManager->getRepository(City::class)->findAll();
         return $this->render($this->themeResolver->getThemePathPrefix("/core/home.html.twig"), [
             "rooms" => $rooms,
+            "searchForm" => $searchForm,
+            "cities" => $cities
         ]);
     }
 }
