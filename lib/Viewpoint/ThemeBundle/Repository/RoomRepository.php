c4db2ccd3fd69454ea0edf59b8e053d70e01d85c
xdark-git <konteyemouhamadou@gmail.com>
2023-08-18 18:51:21 +0100

    search and sort implementation

--- lib/Viewpoint/ThemeBundle/Repository/RoomRepository.php
+++ lib/Viewpoint/ThemeBundle/Repository/RoomRepository.php
@@ -7,6 +7,8 @@ use Viewpoint\ThemeBundle\Entity\Room;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\Query;
 use Doctrine\Persistence\ManagerRegistry;
+use Viewpoint\ThemeBundle\Data\SearchData;
+use Viewpoint\ThemeBundle\Entity\City;
 
 /**
  * @extends ServiceEntityRepository<Room>
@@ -41,7 +43,62 @@ class RoomRepository extends ServiceEntityRepository
         }
     }
 
-    public function findAvailableRoomsQuery():Query
+    public function findAvailableRoomsQuery(
+        ?SearchData $search = null,
+        ?string $sortData = null
+    ): Query {
+        $currentDateTime = new \DateTime();
+
+        $qb = $this->createQueryBuilder("r")
+            ->andWhere("r.departureDate > :currentDateTime")
+            ->andWhere("r.isDeleted = :isDeleted")
+            ->innerJoin("r.user", "u") // Join with the user entity
+            ->andWhere("u.isDeleted = :isUserDeleted")
+            ->setParameter("currentDateTime", $currentDateTime)
+            ->setParameter("isDeleted", false)
+            ->setParameter("isUserDeleted", false)
+            ->orderBy("r.departureDate", "ASC");
+
+        if ($search) {
+            if ($search->departureLocation) {
+                $qb->andWhere(
+                    "r.departureLocation IN (SELECT c.id FROM " .
+                        City::class .
+                        " c WHERE c.name LIKE :departureLocation)"
+                )->setParameter("departureLocation", "%" . $search->departureLocation . "%");
+            }
+
+            if ($search->arrivalLocation) {
+                $qb->andWhere(
+                    "r.arrivalLocation IN (SELECT c.id FROM " .
+                        City::class .
+                        " c WHERE c.name LIKE :arrivalLocation)"
+                )->setParameter("arrivalLocation", "%" . $search->arrivalLocation . "%");
+            }
+
+            if ($search->minDate) {
+                $qb->andWhere("r.departureDate >= :minDate")->setParameter(
+                    "minDate",
+                    $search->minDate
+                );
+            }
+
+            if ($search->maxDate) {
+                $maxDate = $search->maxDate->setTime(23, 59, 59);
+                $qb->andWhere("r.departureDate <= :maxDate")->setParameter("maxDate", $maxDate);
+            }
+        }
+
+        if ($sortData === "cheaper") {
+            $qb->orderBy("r.unitPrice", "ASC");
+        } elseif ($sortData === "weight") {
+            $qb->orderBy("r.weight", "DESC");
+        }
+
+        return $qb->getQuery();
+    }
+
+    public function findAvailableRoomForHomePage(int $limit = 3)
     {
         $currentDateTime = new \DateTime();
 
@@ -53,23 +110,8 @@ class RoomRepository extends ServiceEntityRepository
             ->setParameter("currentDateTime", $currentDateTime)
             ->setParameter("isDeleted", false)
             ->setParameter("isUserDeleted", false)
-            ->orderBy('r.departureDate', 'ASC') 
-            ->getQuery();
-    }
-
-    public function findAvailableRoomForHomePage(int $limit = 3){
-        $currentDateTime = new \DateTime();
-
-        return $this->createQueryBuilder("r")
-            ->andWhere("r.departureDate > :currentDateTime")
-            ->andWhere("r.isDeleted = :isDeleted")
-            ->innerJoin("r.user", "u") // Join with the user entity
-            ->andWhere("u.isDeleted = :isUserDeleted")
-            ->setParameter("currentDateTime", $currentDateTime)
-            ->setParameter("isDeleted", false)
-            ->setParameter("isUserDeleted", false)
-            ->orderBy('r.departureDate', 'ASC') 
-            ->setMaxResults($limit) 
+            ->orderBy("r.departureDate", "ASC")
+            ->setMaxResults($limit)
             ->getQuery()
             ->getResult();
     }
