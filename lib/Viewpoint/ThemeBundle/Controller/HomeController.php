92500ad07c8fad60e7e737f1038341d14c699d98
xDev <papesock@yahoo.fr>
2023-08-19 16:49:16 +0200

    sending email

--- lib/Viewpoint/ThemeBundle/Controller/HomeController.php
+++ lib/Viewpoint/ThemeBundle/Controller/HomeController.php
@@ -3,11 +3,15 @@
 namespace Viewpoint\ThemeBundle\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Viewpoint\ThemeBundle\Service\ThemeResolver;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Viewpoint\ThemeBundle\Data\ContactData;
 use Viewpoint\ThemeBundle\Entity\City;
 use Viewpoint\ThemeBundle\Entity\Room;
 use Viewpoint\ThemeBundle\Form\ContactFormType;
@@ -40,13 +44,37 @@ class HomeController extends AbstractController
     }
 
     #[Route('/contact', name: 'app_contact')]
-    public function contact(Request $request): Response
+    public function contact(Request $request, MailerInterface $mailer): Response
     {
         $contactForm = $this->createForm(ContactFormType::class);
         $contactForm->handleRequest($request);
 
         if($contactForm->isSubmitted() && $contactForm->isValid()){
-            dd($contactForm->getData());
+            /** @var ContactData */ 
+            $submittedData = $contactForm->getData();
+    
+            try{
+                $email = (new TemplatedEmail())
+                    ->from("no-reply@gp-tracker.com")
+                    ->to("contact@gp-tracker.com")
+                    ->subject("[GP-TRACKER CONTACT] ".$submittedData->object)
+                    ->htmlTemplate($this->themeResolver->getThemePathPrefix('/email/contact.html.twig'))
+                    ->context([
+                        'firstname' => $submittedData->firstname,
+                        'lastname' => $submittedData->lastname,
+                        'userEmail' => $submittedData->email,
+                        'subject' => $submittedData->object,
+                        'message' => $submittedData->message
+                    ]);
+
+                $mailer->send($email);
+
+                $this->addFlash("success", "Votre message a été tres bien envoyé.");
+            }
+            catch(\Exception $e){
+                $this->addFlash("error", "Oups, une erreur est survenue!!!");
+            }
+            
         }
 
         return $this->render($this->themeResolver->getThemePathPrefix('/core/contact.html.twig'), [
