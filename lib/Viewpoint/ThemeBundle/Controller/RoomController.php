6f6322ddff4d6630f82c2c9100eb69a91e3936a8
xdark-git <konteyemouhamadou@gmail.com>
2023-08-22 12:15:13 +0100

    implementing update and delete method

--- lib/Viewpoint/ThemeBundle/Controller/RoomController.php
+++ lib/Viewpoint/ThemeBundle/Controller/RoomController.php
@@ -96,54 +96,127 @@ class RoomController extends AbstractController
         );
     }
 
-    #[Route("/{slug}", name: "app_room_detail", methods: ["GET"])]
-    public function show(string $slug, EntityManagerInterface $entityManager)
+    #[Route("/modification/{slug}", name: "app_room_update")]
+    public function update(string $slug, Request $request): Response
     {
-        /** @var Room  */
-        $room = $entityManager->getRepository(Room::class)->findOneBy(["slug" => $slug]);
+        /** @var Room */
+        $room = $this->entityManager
+            ->getRepository(Room::class)
+            ->findOneBy(["slug" => $slug, "isDeleted" => false]);
 
         if (!$room) {
             throw $this->createNotFoundException("Page Introuvable");
         }
-        
-        if($room->getUser() != $this->getUser()) 
-        {
+
+        // Check if the current user is the owner of the room
+        if ($room->getUser() !== $this->getUser()) {
+            throw $this->createAccessDeniedException("You are not authorized to update this room.");
+        }
+
+        $form = $this->createForm(RoomFormType::class, $room);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Update the room
+            $this->entityManager->flush();
+
+            $newSlug = $room->getSlug();
+
+            $this->addFlash("success", "Votre annonce a été mise à jour avec succès");
+
+            return $this->redirectToRoute("app_room_detail", ["slug" => $newSlug]);
+        }
+
+        $cities = $this->entityManager->getRepository(City::class)->findAll();
+
+        return $this->render(
+            $this->themeResolver->getThemePathPrefix("/core/room-creation.html.twig"),
+            [
+                "roomCreationForm" => $form->createView(),
+                "cities" => $cities,
+            ]
+        );
+    }
+
+    #[Route("/suppression/{slug}", name: "app_room_delete", methods: ["POST"])]
+    public function delete(string $slug, Request $request): Response
+    {
+        /** @var Room */
+        $room = $this->entityManager->getRepository(Room::class)->findOneBy(["slug" => $slug]);
+
+        if (!$room) {
+            throw $this->createNotFoundException("Page Introuvable");
+        }
+
+        // Check if the current user is the owner of the room or isAdmin
+        if ($room->getUser() !== $this->getUser() && !$this->isGranted("ROLE_ADMIN")) {
+            throw $this->createAccessDeniedException("Accès non autorisé");
+        }
+
+        $submittedToken = $request->request->get("token");
+
+        if ($this->isCsrfTokenValid("delete-room", $submittedToken)) {
+            $room->setIsDeleted(true);
+            $this->entityManager->flush();
+
+            $this->addFlash("success", "Votre annonce a été supprimée avec succès");
+
+            return $this->redirectToRoute("app_user_package");
+        }
+
+        // This code should never be reached, but it's good to include as a fallback
+        throw new \Exception("Token introuvable");
+    }
+
+    #[Route("/{slug}", name: "app_room_detail", methods: ["GET"])]
+    public function show(string $slug, EntityManagerInterface $entityManager)
+    {
+        /** @var Room  */
+        $room = $entityManager
+            ->getRepository(Room::class)
+            ->findOneBy(["slug" => $slug, "isDeleted" => false]);
+
+        if (!$room) {
+            throw $this->createNotFoundException("Page Introuvable");
+        }
+
+        if ($room->getUser() != $this->getUser()) {
             $currentDateTime = new \DateTime();
 
             /** @var RoomViewsHistory */
-            $userLastTimeVisited= $entityManager
+            $userLastTimeVisited = $entityManager
                 ->getRepository(RoomViewsHistory::class)
                 ->findOneBy([
                     "room" => $room,
-                    "user"=> $this->getUser()
+                    "user" => $this->getUser(),
                 ]);
-                
-            if(!$userLastTimeVisited)
-            {
-                $newViewer = (new RoomViewsHistory)
+
+            if (!$userLastTimeVisited) {
+                $newViewer = (new RoomViewsHistory())
                     ->setUser($this->getUser())
                     ->setRoom($room)
                     ->setLastTimeVisited($currentDateTime);
 
                 $entityManager->persist($newViewer);
                 $entityManager->flush();
-            }else{
+            } else {
                 $userLastTimeVisited->setLastTimeVisited($currentDateTime);
                 $entityManager->flush();
             }
         }
-        $viewHistory = $entityManager->getRepository(RoomViewsHistory::class)->findBy(["room" => $room]);
+        $viewHistory = $entityManager
+            ->getRepository(RoomViewsHistory::class)
+            ->findBy(["room" => $room]);
         $viewCount = count($viewHistory);
         $roomName = $room->getName();
-    
+
         return $this->render(
             $this->themeResolver->getThemePathPrefix("/core/room-detail.html.twig"),
             [
                 "room" => $room,
                 "viewCount" => $viewCount,
-                "roomName" => $roomName
+                "roomName" => $roomName,
             ]
         );
     }
-    
 }
